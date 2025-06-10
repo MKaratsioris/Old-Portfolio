834b874d664b66ade78edbc98f7202b386971e13
michaliskaratsiorispartner <michalis.karatsioris@partner.bmwgroup.com>
2025-06-10 20:52:18 +0200

    fixed Portfolio page error

--- src/pages/AllPages.js
+++ src/pages/AllPages.js
@@ -1,6 +1,5 @@
 export { About } from "./About";
 export { Contact } from "./Contact";
 export { Home } from "./Home";
-export { Portfolio } from "./Portfolio";
 export { Resume } from "./Resume";
 export { Skills } from "./Skills";
